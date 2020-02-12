3f07fa41ea3f484b7e74c930b0b7ea65a356c3bb
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2020-02-11 21:05:07 -0500

    Player can now create moving platforms

---
+++ Week 6 Playtest/Assets/Throw.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+	public GameObject platform;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			GameObject clone = GameObject.Instantiate(platform, transform.position, platform.transform.rotation);
+
+			clone.GetComponent<Rigidbody>().AddForce(Vector3.forward * 100/*, ForceMode.Impulse*/);
+		}
+
+	}
+}
